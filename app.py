e660c3a3bf40858ff8c0cddef90b2c31676d4785
vandanrohatgi <vandanrohatgi@gmail.com>
2020-11-21 18:52:02 +0530

    added newscan page

--- app.py
+++ app.py
@@ -4,7 +4,15 @@ app=Flask(__name__,static_url_path='',static_folder='web/static',template_folder
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('history.html')
+
+@app.route('/newscan.html')
+def newscan():
+    return render_template('newscan.html')
+
+@app.route('/history.html')
+def history():
+    return render_template('history.html')
 
 if __name__ == "__main__":
     app.run()
