55b59e5e3b722423abb0f59a3ee29bd53e507218
vandanrohatgi <vandanrohatgi@gmail.com>
2021-01-28 14:59:48 +0530

    improved ssl module

--- modules/sslModule.py
+++ modules/sslModule.py
@@ -5,6 +5,7 @@ from cryptography.hazmat.backends.openssl import backend
 from OpenSSL import crypto
 import pickle
 from db import insert
+import re
 
 class sslModule:
     def __init__(self,uuid,name,target,timestamp,connection):
@@ -13,7 +14,7 @@ class sslModule:
         self.target=target
         self.connection=connection
         self.timestamp=timestamp
-        self.ip=socket.gethostbyname(self.target)
+        self.ip=socket.gethostbyname(target)
         self.collectedData={}
 
     def start(self):
@@ -21,29 +22,16 @@ class sslModule:
         encodedCert=bytes(certpem,'utf-8')
         cert = x509.load_pem_x509_certificate(encodedCert, backend)
         certload=crypto.load_certificate(crypto.FILETYPE_PEM, encodedCert)
-        rawData=crypto.dump_certificate(crypto.FILETYPE_TEXT,certload)
-        output={'Issuer':str(cert.issuer)[1:-1],"Issued To":str(cert.subject)[1:-1],"Validity":str(cert.not_valid_before)+" to "+str(cert.not_valid_after),"Raw Data":str(rawData.decode('utf-8'))}
-
-        byteData=pickle.dumps(output)
+        rawData=str(crypto.dump_certificate(crypto.FILETYPE_TEXT,certload).decode('utf-8'))
+        self.collectedData["Common Name"]=re.findall("CN=(.*?)\)",str(cert.subject))[0]
+        self.collectedData["Serial Number"]=str(cert.serial_number)
+        self.collectedData["Valid From"]=str(cert.not_valid_before)
+        self.collectedData["Valid Till"]=str(cert.not_valid_after)
+        self.collectedData["Public Key"]=cert.public_key().__class__.__name__
+        self.collectedData["Issuer"]=re.findall("CN=(.*?)\)",str(cert.issuer))[0]
+        #self.collectedData["Signature Algorithm"]=re.findall("Signature Algorithm: (.*?)",rawData)[0]
+        self.collectedData["Signature Algorithm"]=cert.signature_hash_algorithm.__class__.__name__
+        self.collectedData["Raw Certificate"]=rawData
+        #output={'Issuer':str(cert.issuer)[1:-1],"Issued To":str(cert.subject)[1:-1],"Validity":str(cert.not_valid_before)+" to "+str(cert.not_valid_after),"Raw Data":str(rawData.decode('utf-8'))}
+        byteData=pickle.dumps(self.collectedData)
         insert(self.uuid,self.name,self.target,self.timestamp,'sslModule',byteData,self.connection)
-        '''cursor=self.connection.cursor()
-        cursor.execute("INSERT INTO output VALUES (?,?,?,?,'sslModule',?)",(self.uuid,self.name,self.target,self.timestamp,byteData))
-        self.connection.commit()'''
-
-
-'''con=sqlite3.connect("/home/vandan/osint/osint.db")
-obj=sslModule('123','test','grocio.in',con)
-obj.start()'''
-
-
-'''ctx = ssl.create_default_context()
-    with ctx.wrap_socket(socket.socket(), server_hostname=target) as s:
-        s.connect((target, 443))
-        cert = s.getpeercert(True)
-    demcert=ssl.DER_cert_to_PEM_cert(cert)'''
-
-'''subject = dict(x[0] for x in cert['subject'])
-issued_to = subject['commonName']
-issuer = dict(x[0] for x in cert['issuer'])
-issued_by = issuer['commonName']
-print(issued_to,issued_by)'''
