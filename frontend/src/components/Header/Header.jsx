ef191d706971c7fd064ff188aeddac1aabfb3066
sachin <sachinnegi808@gmail.com>
2022-04-14 15:20:50 +0530

    frontend set up

---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Header() {
+  return <h1 className="text-dark text-center">React With Bootstrap</h1>;
+}
+
+export default Header;
---
+++ frontend/src/config.js
@@ -0,0 +1,1 @@
+export const BASE_URL = process.env.APIHost || "http://localhost:5000";
