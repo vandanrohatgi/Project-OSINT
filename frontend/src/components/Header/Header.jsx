b7f019eef58a1b6beb486311e9865145bb1037db
sachin <sachinnegi808@gmail.com>
2022-04-19 21:02:22 +0530

    integrate single test api

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,6 +1,19 @@
 import React from "react";
+import { useEffect } from "react";
+import { get } from "../../utils/axios";
 
 function Header() {
+  useEffect(() => {
+    get("/")
+      .then((data) => {
+        console.log(data);
+        alert(JSON.stringify(data));
+      })
+      .catch((err) => {
+        console.log(err);
+        alert(err.message);
+      });
+  }, []);
   return <h1 className="text-dark text-center">React With Bootstrap</h1>;
 }
 
--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,1 +1,2 @@
-export const BASE_URL = process.env.APIHost || "http://localhost:5000";
+export const BASE_URL =
+  process.env.REACT_APP_API_HOST || "http://localhost:5000";
