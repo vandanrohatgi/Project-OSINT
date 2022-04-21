8b57cfb786ff73b92cff80e1ec122ad539a27efb
SWAPNIL MISHRA <42887861+swapnil14912@users.noreply.github.com>
2022-04-21 18:59:12 +0530

    Update axios.js

--- frontend/src/utils/axios.js
+++ frontend/src/utils/axios.js
@@ -1,15 +1,15 @@
-import axios from "axios/index";
-import { BASE_URL } from "../config";
+// import axios from "axios/index";
+// import { BASE_URL } from "../config";
 
-const axiosInstance = axios.create({});
+// const axiosInstance = axios.create({});
 
-export const get = (path, params = {}) =>
-  axiosInstance.get(BASE_URL + path, params, {
-    params,
-  });
+// export const get = (path, params = {}) =>
+//   axiosInstance.get(BASE_URL + path, params, {
+//     params,
+//   });
 
-export const post = (path, params = {}) =>
-  axiosInstance.post(BASE_URL + path, params);
+// export const post = (path, params = {}) =>
+//   axiosInstance.post(BASE_URL + path, params);
 
-export const put = (path, params = {}) =>
-  axiosInstance.put(BASE_URL + path, params);
+// export const put = (path, params = {}) =>
+//   axiosInstance.put(BASE_URL + path, params);
