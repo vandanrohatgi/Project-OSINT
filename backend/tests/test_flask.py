fe6426e81dea94e1ac4671253a948747bdcc4da6
vandanrohatgi <vandanrohatgi@gmail.com>
2022-04-22 22:05:25 +0530

    Added basic tests

--- backend/tests/test_flask.py
+++ backend/tests/test_flask.py
@@ -1,7 +1,29 @@
-import pytest
+import json
+from unittest.mock import patch
 
 def test_index(client):
     response=client.get("/")
-    #print(response.json)
-    #assert response.json["msg"]==""
     assert response.status_code==200
+
+def test_index_response(client):
+    response=client.get("/")
+    assert response.json["msg"]=="API works!"
+
+def test_auth(client):
+    response=client.post("/login",data=json.dumps({"username":"test","password":"test"}),content_type="application/json")
+    assert response.status_code==200
+    assert 'access_token' in response.json.keys()
+
+def test_unauth(client):
+    response=client.get("/logout")
+    assert response.status_code == 401
+    response=client.get("/getScanInfo")
+    assert response.status_code == 401
+    response=client.post("/start")
+    assert response.status_code == 401
+
+@patch('flask_jwt_extended.view_decorators.verify_jwt_in_request')
+def test_get_scan_info(mock_jwt_token,client):
+    response=client.get("/getScanInfo")
+    assert response.status_code == 200
+
