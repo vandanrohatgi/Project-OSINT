b081ab4cd5b6110ed16007ca72492f371a51eb04
sachin <sachinnegi808@gmail.com>
2022-04-24 15:32:57 +0530

    add Login comp with particles bg animaton

--- frontend/src/pages/Login/Login.jsx
+++ frontend/src/pages/Login/Login.jsx
@@ -1,9 +1,78 @@
-import React from 'react'
+import React, { useState } from "react";
+import ParticleBackground from "../../components/Login/ParticleBackground";
+import { Form, Button } from "react-bootstrap";
+import styles from "./login.module.css";
+import { MdOutlineSecurity } from "react-icons/md";
+import Input from "../../components/Login/Input";
+import { post } from "../../utils/axios";
+import { useNavigate } from "react-router-dom";
 
 function Login() {
+  const navigate = useNavigate();
+
+  const [credentials, setCredentials] = useState({
+    username: "",
+    password: "",
+  });
+
+  const [isSubmitting, setIsSubmitting] = useState(false);
+
+  function handleChange(e) {
+    setCredentials((creds) => ({ ...creds, [e.target.name]: e.target.value }));
+  }
+  async function handleSubmit(e) {
+    e.preventDefault();
+    setIsSubmitting(true);
+
+    await post("/login", credentials)
+      .then(({ data }) => {
+        sessionStorage.setItem("accessToken", data.access_token);
+        navigate("/dashboard");
+      })
+      .catch((err) => {
+        console.log(err);
+        alert(err.message);
+      })
+      .finally(() => {
+        setIsSubmitting(false);
+      });
+  }
   return (
-    <div>Login</div>
-  )
+    <div>
+      <div className={styles.LoginFormContainer}>
+        <div className={styles.AuthIcon}>
+          <MdOutlineSecurity />
+        </div>
+        <Form onSubmit={handleSubmit}>
+          <Input
+            value={credentials.username}
+            onChange={handleChange}
+            placeholder={"Enter Username"}
+            name="username"
+            required
+          />
+          <Input
+            value={credentials.password}
+            onChange={handleChange}
+            placeholder={"Enter Password"}
+            name="password"
+            type="password"
+            required
+          />
+
+          <Button
+            variant="success"
+            type="submit"
+            className="d-block mx-auto px-3"
+            disabled={isSubmitting}
+          >
+            {isSubmitting ? "Submitting..." : "Submit"}
+          </Button>
+        </Form>
+      </div>
+      <ParticleBackground />
+    </div>
+  );
 }
 
-export default Login
+export default Login;
