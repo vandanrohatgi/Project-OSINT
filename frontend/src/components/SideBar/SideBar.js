ed26cce3a4aa38ac820f80c0d918e30d458e0f8f
sachin <sachinnegi808@gmail.com>
2022-05-08 13:07:51 +0530

    logout func

--- frontend/src/components/SideBar/SideBar.js
+++ frontend/src/components/SideBar/SideBar.js
@@ -1,31 +1,40 @@
-import {CDBSidebar, CDBSidebarContent, CDBSidebarHeader, CDBSidebarMenu, CDBSidebarMenuItem} from 'cdbreact'; 
+import {
+  CDBSidebar,
+  CDBSidebarContent,
+  CDBSidebarHeader,
+  CDBSidebarMenu,
+  CDBSidebarMenuItem,
+} from "cdbreact";
+import { useNavigate } from "react-router-dom";
 
-const Sidebar=()=>{
-    return (
-        <div style={{display:'flex',}}>
-            <CDBSidebar textColor="#000000" backgroundColor="#f8f8f8">
-                <CDBSidebarHeader prefix={<i className="fa fa-bars fa-large"></i>} style={{height:"55px"}}>
-                    Dashboard
-                </CDBSidebarHeader>
-                <CDBSidebarContent className="sidebar-content">
-                    <CDBSidebarMenu>
-                            <CDBSidebarMenuItem icon="search">
-                                New Scan
-                            </CDBSidebarMenuItem>
-                            <CDBSidebarMenuItem icon="history">
-                                History
-                            </CDBSidebarMenuItem>
-                            <CDBSidebarMenuItem icon="columns">
-                                Overview
-                            </CDBSidebarMenuItem>
-                             <CDBSidebarMenuItem icon="user">
-                                LogOut
-                            </CDBSidebarMenuItem>
-                    </CDBSidebarMenu>
-                </CDBSidebarContent>
-            </CDBSidebar>
-        </div>
-    )
-}
+const Sidebar = () => {
+  const navigate = useNavigate();
+  function handleLogout() {
+    sessionStorage.removeItem("accessToken");
+    navigate(0);
+  }
+  return (
+    <div style={{ display: "flex" }}>
+      <CDBSidebar textColor="#000000" backgroundColor="#f8f8f8">
+        <CDBSidebarHeader
+          prefix={<i className="fa fa-bars fa-large"></i>}
+          style={{ height: "55px" }}
+        >
+          Dashboard
+        </CDBSidebarHeader>
+        <CDBSidebarContent className="sidebar-content">
+          <CDBSidebarMenu>
+            <CDBSidebarMenuItem icon="search">New Scan</CDBSidebarMenuItem>
+            <CDBSidebarMenuItem icon="history">History</CDBSidebarMenuItem>
+            <CDBSidebarMenuItem icon="columns">Overview</CDBSidebarMenuItem>
+            <CDBSidebarMenuItem icon="user" onClick={handleLogout}>
+              LogOut
+            </CDBSidebarMenuItem>
+          </CDBSidebarMenu>
+        </CDBSidebarContent>
+      </CDBSidebar>
+    </div>
+  );
+};
 
 export default Sidebar;
